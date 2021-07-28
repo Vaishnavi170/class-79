9a995759ce8faf6d327cd3ad41f601b861e4ec50
Vaishnavi170 <84765083+Vaishnavi170@users.noreply.github.com>
2021-07-28 16:45:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var name_of_student_array=[];
+function submit(){
+    var name_1=document.getElementById("student_name1").value;
+    var name_2=document.getElementById("student_name2").value;
+    var name_3=document.getElementById("student_name3").value;
+    var name_4=document.getElementById("student_name4").value;
+name_of_student_array.push(name_1);
+name_of_student_array.push(name_2);
+name_of_student_array.push(name_3);
+name_of_student_array.push(name_4);
+console.log(name_of_student_array);
+document.getElementById("display_names").innerHTML=name_of_student_array;
+document.getElementById("submit_button").style.display="none";
+document.getElementById("sort_button").style.display="inline-block";
+}
+function sort(){
+    name_of_student_array.sort();
+    console.log(name_of_student_array);
+    document.getElementById("display_names").innerHTML=name_of_student_array;
+}
